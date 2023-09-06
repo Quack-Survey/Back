8b2c2456318f6a6b7c73b6464f2238cc054cca1a
jungyeonwoo <oscar0421@kakao.com>
2023-09-06 14:14:25 +0900

    Feat api path intro추가

--- routes/api.js
+++ routes/api.js
@@ -2,4 +2,8 @@ const router = require("express").Router();
 
 router.use("/admin", require("./admin"));
 
+router.get("/", (req, res) => {
+  res.send("hello this is quacksurvey's api server");
+});
+
 module.exports = router;
