7ffea3fe7fd51ddcc8ecab1842aa69ce6adecbf8
jungyeonwoo <oscar0421@kakao.com>
2023-09-15 15:49:11 +0900

    Fix: mongoose 관련 경고 해결

--- app.js
+++ app.js
@@ -18,7 +18,10 @@ app.use(cors({ origin: true, credentials: true }));
 mongoose.Promise = global.Promise;
 
 mongoose
-  .connect(process.env.MONGO_URI, {})
+  .connect(process.env.MONGO_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
   .then(() => console.log("Successfully connected to mongodb"))
   .catch((e) => console.error(e));
 
