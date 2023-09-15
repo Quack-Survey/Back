2c8c3f67788218c5607bb7e72cae48e0ac0f9e31
Hun5LEE <118670781+Hun5LEE@users.noreply.github.com>
2023-09-15 15:07:55 +0900

    Update formContent.js

--- routes/formContent.js
+++ routes/formContent.js
@@ -4,12 +4,7 @@ const formContent = require("../models/formContent");
 
 router.get("/", async (req, res) => {
   try {
-    const newFormContent = await formContent.findAll({
-      text: "테스트 텍스트입니다.",
-      select: [""],
-      templateId: "65029a6e13cf637226a2b976",
-      formId: "6502b54cfc647197a4645d26",
-    });
+    const newFormContent = await formContent.findAll({});
     res.status(200).json(newFormContent);
   } catch (err) {
     console.error("Error creating formContent:", err);
