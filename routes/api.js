0f87f9feb8e76264124eb9c4396cd83c210228f9
jungyeonwoo <oscar0421@kakao.com>
2023-09-15 15:44:52 +0900

    Chore: 라우터 경로 수정

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-router.use("/users", require("./user"));
+router.use("/users", require("./users"));
 router.use("/form", require("./form"));
 router.use("/formContent", require("./formContent"));
 router.use("/template", require("./template"));
