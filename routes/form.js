a1d1c7cfd38bf38092762f7dcd0965c5b7ca0492
Hun5LEE <118670781+Hun5LEE@users.noreply.github.com>
2023-09-15 15:12:49 +0900

    Update form.js

--- routes/form.js
+++ routes/form.js
@@ -5,7 +5,7 @@ const form = require("../models/form");
 router.get("/", async (req, res) => {
   try {
     const newForm = await form.findAll({});
-    res.status(201).json(newForm);
+    res.status(200).json(newForm);
   } catch (err) {
     console.error("Error creating form:", err);
     res.status(500).json({ error: "Failed to create form" });
