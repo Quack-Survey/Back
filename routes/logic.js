6ee79c9017f6ddcc5e68ec26a7e72f6b8f9e5de3
Hun5LEE <118670781+Hun5LEE@users.noreply.github.com>
2023-09-15 15:13:09 +0900

    Update logic.js

--- routes/logic.js
+++ routes/logic.js
@@ -5,7 +5,7 @@ const logic = require("../models/logic");
 router.get("/", async (req, res) => {
   try {
     const newLogic = await logic.findAll({});
-    res.status(201).json(newLogic);
+    res.status(200).json(newLogic);
   } catch (err) {
     console.error("Error creating logic:", err);
     res.status(500).json({ error: "Failed to create logic" });
