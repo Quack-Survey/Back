8e8abc8ed1bbc9cf9700efc09f03dfced50b7e01
Hun5LEE <jhlee091697@gmail.com>
2023-09-15 23:51:58 +0900

    Feat: templateOption CRUD 기능구현

--- routes/templateOption.js
+++ routes/templateOption.js
@@ -4,11 +4,46 @@ const templateOption = require("../models/templateOption");
 
 router.get("/", async (req, res) => {
   try {
-    const templateOptionData = await templateOption.findAll({});
+    const templateOptionData = await templateOption.findAll({
+      templateId: req.query.templateId,
+    });
     res.status(200).json(templateOptionData);
   } catch (err) {
     console.error("Error getting templateOption:", err);
     res.status(500).json({ error: "Failed to get templateOption" });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    await templateOption.create(req.body);
+    res.status(201).json(true);
+  } catch (err) {
+    console.error("Error creating templateOption:", err);
+    res.status(500).json({ error: "Failed to create templateOption" });
+  }
+});
+
+router.patch("/", async (req, res) => {
+  try {
+    await templateOption.updateOne(
+      { templateId: req.query.templateId },
+      req.body,
+    );
+    res.status(200).json(true);
+  } catch (err) {
+    console.error("Error updating templateOption:", err);
+    res.status(500).json({ error: "Failed to update templateOption" });
+  }
+});
+
+router.delete("/", async (req, res) => {
+  try {
+    await templateOption.delete(req.body);
+    res.status(200).json(true);
+  } catch (err) {
+    console.error("Error deleting templateOption:", err);
+    res.status(500).json({ error: "Failed to delete templateOption" });
   }
 });
 
