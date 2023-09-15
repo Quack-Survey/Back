33f24e425a21db8d1f9219ccc4aa664ec6ac6017
Hun5LEE <118670781+Hun5LEE@users.noreply.github.com>
2023-09-15 15:08:29 +0900

    Update form.js

--- routes/form.js
+++ routes/form.js
@@ -4,12 +4,7 @@ const form = require("../models/form");
 
 router.get("/", async (req, res) => {
   try {
-    const newForm = await form.findAll({
-      title: "테스트 폼",
-      type: "text",
-      order: 1,
-      templateId: "65029a6e13cf637226a2b976",
-    });
+    const newForm = await form.findAll({});
     res.status(201).json(newForm);
   } catch (err) {
     console.error("Error creating form:", err);
@@ -19,13 +14,6 @@ router.get("/", async (req, res) => {
 
 router.put("/", async (req, res) => {
   try {
-    // console.log(req.body);
-    // await Promise.all(
-    //   req.body.map(async (a, i) => {
-    //     await form.updateOne({ _id: a._id }, a);
-    //   }),
-    // );
-
     res.send("hello");
   } catch (err) {
     console.error("Error creating form:", err);
