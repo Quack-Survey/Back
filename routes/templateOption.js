99827ad2b385cf306638796f6792b46934447bae
Hun5LEE <118670781+Hun5LEE@users.noreply.github.com>
2023-09-15 15:13:50 +0900

    Update templateOption.js

--- routes/templateOption.js
+++ routes/templateOption.js
@@ -5,7 +5,7 @@ const templateOption = require("../models/templateOption");
 router.get("/", async (req, res) => {
   try {
     const newTemplateOption = await templateOption.findAll({});
-    res.status(201).json(newTemplateOption);
+    res.status(200).json(newTemplateOption);
   } catch (err) {
     console.error("Error creating templateOption:", err);
     res.status(500).json({ error: "Failed to create templateOption" });
