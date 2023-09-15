cc9a9937c3d1f84e6b2b2cdb885b811411cc9d52
Hun5LEE <118670781+Hun5LEE@users.noreply.github.com>
2023-09-15 15:07:36 +0900

    Update logic.js

--- routes/logic.js
+++ routes/logic.js
@@ -4,12 +4,7 @@ const logic = require("../models/logic");
 
 router.get("/", async (req, res) => {
   try {
-    const newLogic = await logic.findAll({
-      type: "filter",
-      selector: ["1번문항"],
-      formId: "6502b54cfc647197a4645d26",
-      templateId: "65029a3913cf637226a2b970",
-    });
+    const newLogic = await logic.findAll({});
     res.status(201).json(newLogic);
   } catch (err) {
     console.error("Error creating logic:", err);
