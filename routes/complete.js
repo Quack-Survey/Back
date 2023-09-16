a7b6cb623f9ef14d0fe5416c31c63af05fa5f04a
Sungjae.Kwak <sungjae8970@gmail.com>
2023-09-17 02:21:52 +0900

    Chore : delete log and comment

--- routes/complete.js
+++ routes/complete.js
@@ -7,20 +7,10 @@ require("../models/template");
 router
   .route("")
   .get(async (req, res) => {
-    // "complete/?userId=ObjectId"
-    // "complete/?templateId=ObjectId"
     try {
       const { userId, templateId } = req.query;
-      // 특정 유저의 모든 응답데이터 조회
       if (userId) {
-        // const completes = {};
         const templatesId = await Template.find({ userId }, "_id");
-        // console.log(templatesId);
-        // [
-        //   { _id: 64fc5a9458a018020854ff2b },
-        //   { _id: 6503d7e7871261b5bb71e12a },
-        //   { _id: 6503d7ed871261b5bb71e12b }
-        // ]
         const completes = await Promise.all(
           templatesId.map(
             async ({ _id }) =>
@@ -29,7 +19,6 @@ router
         );
         res.json(completes.map((arr) => (arr.length === 0 ? [null] : arr)));
       }
-      // 특정 설문조사의 모든 응답 데이터 조회
       if (templateId) {
         const completes = await Complete.find({ templateId });
         res.json(completes);
@@ -41,7 +30,6 @@ router
   })
   .post(async (req, res) => {
     const { templateId } = req.query;
-    // 응답자가 설문 진입 시, 미완상태로 데이터 밀어넣기
     try {
       const complete = await Complete.create({
         templateId,
